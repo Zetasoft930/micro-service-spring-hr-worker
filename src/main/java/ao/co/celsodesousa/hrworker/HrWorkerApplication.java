c4d49ee5b0c9e500d8e81d2e311043a3c89275f6
zetasoft930 <celsomateussousa@gmail.com>
2023-11-06 14:51:46 +0100

    primeiro commit

---
+++ src/main/java/ao/co/celsodesousa/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package ao.co.celsodesousa.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
